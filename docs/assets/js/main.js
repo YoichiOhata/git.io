47b9d20960ba0a90f1f44d713a77eed556e63263
YoichiOhata <bsh2066@icloud.com>
2024-09-24 14:05:22 +0900

    Add files via upload

---
+++ docs/assets/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+{
+    document.head.insertAdjacentHTML('afterbegin' , `
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="すき家 綾瀬駅前店の店舗ホームページです">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/sanitize.css">
+    <link rel="stylesheet" href="./assets/css/style.css">    `);
+}
